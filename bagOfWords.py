d73832c10bbd9a795daf6e948ea6f4dd53d09d6b
Arthmarellion <Arthmarellion@hotmail.com>
2017-06-09 00:07:12 -0400

    Bag of words manual test

--- bagOfWords.py
+++ bagOfWords.py
@@ -1,5 +1,7 @@
 import re;
+from sklearn.feature_extraction.text import CountVectorizer;
 from collections import Counter;
+import pandas as pd;
 
 documents = ['Hello, how are you!',
              'Win money, win from home.',
@@ -11,3 +13,13 @@ for document in documents:
   for word in re.split(r'\s+', re.sub(r'[^a-z|\s]', '', document.lower())):
     tokens.append(word);
   print(Counter(tokens));
+
+count_vector = CountVectorizer(token_pattern='(?u)\\b\\w\\w+\\b');
+count_vector.fit(documents);
+print(count_vector.get_feature_names());
+
+doc_array = count_vector.transform(documents).toarray();
+print(doc_array);
+
+frequency_matrix = pd.DataFrame(data=doc_array, columns=count_vector.get_feature_names());
+print(frequency_matrix);
